1dce918cac78e59a4e5626cdcef57a9e0e326e7c
utkudemr <utkuudemr@gmail.com>
2023-08-06 11:37:57 +0300

    project created

---
+++ src/corePackages/Core.Packages/Core.Persistance/Models/Entity.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Core.Persistance.Models
+{
+    public class Entity<TId>
+    {
+        public Entity()
+        {
+            Id = default;
+        }
+
+        public Entity(TId id)
+        {
+            Id = id;
+        }
+        public TId Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int? Creator { get; set; }
+
+        
+    }
+}
---
+++ src/projectLothal/ProjectLothal/Domain/Entities/Starship.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistance.Models;
+
+namespace Domain.Entities
+{
+    public class Starship:Entity<Guid>
+    {
+        public Starship()
+        {
+                
+        }
+
+        public Starship(Guid id,string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
