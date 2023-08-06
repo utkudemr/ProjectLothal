ce4c3043c23880485908fb0e0f1788c6f5a888f7
utkudemr <utkuudemr@gmail.com>
2023-08-06 12:13:23 +0300

    mediatr implemented

---
+++ src/projectLothal/ProjectLothal/Business/Features/Starships/Commands/Create/CreatedStarshipResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Features.Starships.Commands.Create
+{
+    public class CreatedStarshipResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/projectLothal/ProjectLothal/WebApi/Controllers/StarshipsController.cs
@@ -0,0 +1,19 @@
+﻿using Business.Features.Starships.Commands.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StarshipsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateStarshipCommand request)
+        {
+            var response = await Mediator.Send(request);
+            return Ok(response);
+        }
+    }
+}
